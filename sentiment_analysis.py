a72fe52e180283369e1213e04f02dfb72a58165a
Tourixion <marketing@tourix.gr>
2024-11-01 12:55:35 +0100

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -12,7 +12,10 @@ from io import StringIO, BytesIO
 # GitHub Gist API configuration
 GITHUB_TOKEN = os.environ.get('PERSONAL_ACCESS_TOKEN')  # Changed to match your existing token name
 INPUT_GIST_IDS = [os.environ.get('GIST_ID_1'), os.environ.get('GIST_ID_2')]
-OUTPUT_GIST_IDS = [os.environ.get('RESULTS_GIST_ID_1'), os.environ.get('RESULTS_GIST_ID_2')]
+OUTPUT_GIST_IDS = [os.environ.get('RESULTS_GIST_ID_1'),
+    os.environ.get('RESULTS_GIST_ID_2'),
+    os.environ.get('RESULTS_GIST_ID_3'),
+    os.environ.get('RESULTS_GIST_ID_4')]
 
 headers = {
     'Authorization': f'token {GITHUB_TOKEN}',
